6bdc1a63cb096404ece16aaab6c928f6abdd8942
nievesfa <nievesfacorro@gmail.com>
2025-02-07 12:59:52 +0100

    test ok

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,15 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        int dividendo = 3, divisor = 0;
+        try {
+            int res = dividendo / divisor;
+            System.out.println(res);
+        } catch (ArithmeticException e) {
+            System.out.println(" No se puede dividir por 0");
+        } finally {
+            System.out.println(" Programa acabado");
+        }
     }
 }
+
